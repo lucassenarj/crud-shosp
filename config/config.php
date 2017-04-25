d2a5f2359e47155202596e0143ab83b5f05b9594
lucassenarj <lucassena.rj@gmail.com>
2017-04-25 02:11:17 -0300

    Adjustments

--- config/config.php
+++ config/config.php
@@ -6,8 +6,8 @@ header("Content-type: text/html; charset=UTF-8");
 setlocale(LC_ALL, "pt_BR");  
 date_default_timezone_set('America/Sao_Paulo');
 
-define('WWW_ROOT'               , dirname(__DIR__)                                      );
-define('BASEURL'                , DIRECTORY_SEPARATOR                                   );
+define('WWW_ROOT'               , dirname(__DIR__)                      );
+define('BASEURL'                , DIRECTORY_SEPARATOR                   );
 
 define("INCLUDES"               , WWW_ROOT  . "/includes"               );
 define("CLASSES"                , INCLUDES  . "/classes/"               );
